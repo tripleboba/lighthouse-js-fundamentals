142d78dd427003cd2c6ba664529a083b7f11a246
tripleboba <coconuttt1010@gmail.com>
2021-11-13 00:23:02 -0800

    array

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// add root beer to the array
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// console.log first flavour
+console.log(iceCreamFlavours[0]);
+
+// console.log last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// total number of flavours
+console.log(iceCreamFlavours.length);
