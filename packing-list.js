c4cb410ee5c0ae71fa716a460bc920ae515b84fb
tripleboba <coconuttt1010@gmail.com>
2021-11-13 01:47:29 -0800

    add while loop function

--- packing-list.js
+++ packing-list.js
@@ -2,6 +2,14 @@ const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glas
 
 console.log("Kitchen stuff to pack:");
 
+// for loop
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// while loop
+let j = 0;
+while (j < packingList.length) {
+  console.log(packingList[j]);
+  j++;
 }
