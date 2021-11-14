b0c8fa9b0eaf4dd0cf00b11a70686494afced70f
tripleboba <coconuttt1010@gmail.com>
2021-11-14 00:44:09 -0800

    concat 2 single arrays into 1

---
+++ concatArray.js
@@ -0,0 +1,14 @@
+// build a function that concat 2 separate arrays into 1
+
+const concat = (array1, array2) => {
+  let result = array1;
+  for (let value of array2) {
+    result.push(value);
+  }
+  return result;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
